00765343bc8edea4f9a7ba4f28445726a5559ae9
DavidFakunle <dfakunle06@gmail.com>
2026-07-27 22:17:57 -0500

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        '''
+        Understand - Given an array sorted in ascending order that has been rotated , find the min
+                    number in the sorted array
+                input - nums array
+                output - the minimum number in the array
+                constraints - has to be in O(log n) time so binary search
+        Plan-
+            create a left and right pointer
+            while left is less than or equal to right
+                define middle as left + right // 2 to get middle number 
+                if the middle number is greater than the right
+                    this means the smallest value can be found on the rightside
+                else:
+                    its on the left side, so move the right value to equal mid
+            return right value
+        Implement-
+        '''
+        left, right = 0 , len(nums) - 1
+
+        while left < right:
+            mid = (left + right ) // 2
+            if nums[mid] >= nums[right]:# the minimum value appears on the rightside of the array
+                left = mid + 1
+            else:
+                right = mid
+
+        return nums[right]
+                 
